614efd76cf5f776f39dfd961a3b8cca231dc209c
chuckmon <chuckmon@student.21-school.ru>
2025-08-08 20:30:38 +0200

    Fix cubic_spline_interpolator.cc

--- src/algo_trading/models/cubic_spline_interpolator.cc
+++ src/algo_trading/models/cubic_spline_interpolator.cc
@@ -25,8 +25,7 @@ CubicSplineInterpolator::CubicSplineInterpolator(
     }
   }
 
-
-  a_coeffs_.resize(points.size() - 1);  // a_i = y_i
+  a_coeffs_.resize(points.size() - 1);
   b_coeffs_.resize(points.size() - 1);
   c_coeffs_.resize(points.size());  // M_i / 2
   d_coeffs_.resize(points.size() - 1);
@@ -51,11 +50,10 @@ void CubicSplineInterpolator::buildSpline() {
         3.0 * ((ys_[i + 1] - ys_[i]) / h[i] - (ys_[i] - ys_[i - 1]) / h[i - 1]);
   }
 
-
+  // TDMA
   std::vector<double> l(n);
   std::vector<double> mu(n);
   std::vector<double> z(n);
-
 
   l[0] = 1.0;
   mu[0] = 0.0;
@@ -73,8 +71,7 @@ void CubicSplineInterpolator::buildSpline() {
   }
 
   // Обратный проход
-  c_coeffs_[n - 1] =
-      0.0;
+  c_coeffs_[n - 1] = 0.0;
   for (int i = n - 2; i >= 0; --i) {
     c_coeffs_[i] = z[i] - l[i] * c_coeffs_[i + 1];
   }
@@ -89,7 +86,6 @@ void CubicSplineInterpolator::buildSpline() {
     b_coeffs_[i] = (ys_[i + 1] - ys_[i]) / h[i] -
                    h[i] * (c_coeffs_[i + 1] + 2.0 * c_coeffs_[i]) / 3.0;
     d_coeffs_[i] = (c_coeffs_[i + 1] - c_coeffs_[i]) / (3.0 * h[i]);
-
   }
 }
 
@@ -99,23 +95,18 @@ double CubicSplineInterpolator::interpolate(double x_val) const {
     throw std::runtime_error("Interpolation point x is out of data range.");
   }
 
-
   auto it = std::upper_bound(xs_.begin(), xs_.end(), x_val);
   size_t i = std::distance(xs_.begin(), it) - 1;
-
 
   if (i >= xs_.size() - 1) {
     i = xs_.size() - 2;
   }
   if (x_val == xs_.back() && xs_.size() > 1) {
-    i = xs_.size() -
-        2;
+    i = xs_.size() - 2;
   }
 
   double dx = x_val - xs_[i];
-
-
-  return a_coeffs_[i] + b_coeffs_[i] * dx + c_coeffs_[i] / 2.0 * dx * dx +
+  return a_coeffs_[i] + b_coeffs_[i] * dx + c_coeffs_[i] * dx * dx +
          d_coeffs_[i] * dx * dx * dx;
 }
 }  // namespace s21
