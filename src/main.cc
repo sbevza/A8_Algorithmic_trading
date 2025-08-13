2ed63190700b5e2239bb283bc72a2fbeb2e78d7d
chuckmon <chuckmon@student.21-school.ru>
2025-08-13 12:41:00 +0200

    Test MR

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,10 @@
 int main(int argc, char* argv[]) {
 // test test test
+  QApplication a(argc, argv);
+  a.setWindowIcon(QIcon(":/icon/icon.png"));
+  MainWindow w;
+
+  w.show();
+  return a.exec();
+  // Надо всё удалить, это только для теста
 }
